54c14ac9c53337d7e6a322f53d8e5c05546dc6c1
Mo <164212253+Mo-Nimble@users.noreply.github.com>
2024-07-25 13:52:45 +0530

    chore: Add ContactUs and Footer sections to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import AboutUs from "@/components/sections/AboutUs";
+import ContactUs from "@/components/sections/ContactUs";
+import Footer from "@/components/sections/Footer";
 import Founders from "@/components/sections/Founders";
 import Header from "@/components/sections/Header";
 import HeroSection from "@/components/sections/HeroSection";
@@ -7,12 +9,14 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col bg-primary">
 			<Header />
 			<HeroSection />
 			<AboutUs />
 			<Founders />
 			<Product />
+			<ContactUs />
+			<Footer />
 		</main>
 	);
 }
