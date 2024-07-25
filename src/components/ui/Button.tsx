24c8c9538a8ab45257d075331eef2913c756b58b
Mo <164212253+Mo-Nimble@users.noreply.github.com>
2024-07-25 13:53:50 +0530

    feat: Add Link functionality to Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,13 @@
-const Button = () => {
+import Link from "next/link";
+
+const Button = ({text, path}: {text: string, path: string}) => {
     return (
         <div>
-            Enter
+            <Link href={path}>
+            <button className="bg-secondary border border-dark px-4 py-2 md:mx-7 md:my-20 my-10 font-switzer md:text-2xl text-lg font-light w-fit">
+            {text}
+				</button>
+            </Link>
         </div>
     );
 }
